4c003d235e842974915125b75df738d714430be2
sepidekoohfar <sepideh.koohfar@unh.edu>
2022-11-10 15:18:30 -0500

    updating covid

--- data/camel.py
+++ data/camel.py
@@ -42,7 +42,7 @@ class camelFormatter(ElectricityFormatter):
         """Returns fixed model parameters for experiments."""
 
         fixed_params = {
-            'total_time_steps': 4 * 24 + 2 * self.pred_len,
+            'total_time_steps': 8 * 24 + self.pred_len,
             'num_encoder_steps': 4 * 24,
             'num_decoder_steps': self.pred_len,
             'num_epochs': 50,
