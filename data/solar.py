e34fe8e4eea2bb330805e1d14ddc21c55c3a1e44
sepidekoohfar <sepideh.koohfar@unh.edu>
2022-11-15 17:10:57 -0500

    adding denoising autoencoder

--- data/solar.py
+++ data/solar.py
@@ -80,4 +80,4 @@ class SolarFormatter(ElectricityFormatter):
         Returns:
           Tuple of (training samples, validation samples)
         """
-        return 32000, 3840
+        return 3840, 3840
