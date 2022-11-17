9420db63dd3b6bd9bda9790d7ae030436f1bba70
sepidekoohfar <sepideh.koohfar@unh.edu>
2022-11-16 19:47:10 -0500

    denoisnig version 3

--- models/eff_acat.py
+++ models/eff_acat.py
@@ -258,11 +258,13 @@ class process_model(nn.Module):
         super(process_model, self).__init__()
 
         self.encoder = nn.Sequential(nn.Conv1d(in_channels=d, out_channels=d, kernel_size=3, padding=int((3-1)/2)),
+                                     nn.BatchNorm1d(d),
                                      nn.Conv1d(in_channels=d, out_channels=d, kernel_size=9, padding=int((9-1)/2)),
                                      nn.BatchNorm1d(d),
                                      nn.Softmax(dim=-1)).to(device)
 
         self.decoder = nn.Sequential(nn.Conv1d(in_channels=d, out_channels=d, kernel_size=3, padding=int((3-1)/2)),
+                                     nn.BatchNorm1d(d),
                                      nn.Conv1d(in_channels=d, out_channels=d, kernel_size=9, padding=int((9-1)/2)),
                                      nn.BatchNorm1d(d),
                                      nn.Softmax(dim=-1)).to(device)
