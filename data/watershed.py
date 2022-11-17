010fe683fb1849673c466192215a2f175fc38851
sepidekoohfar <sepideh.koohfar@unh.edu>
2022-11-17 09:21:36 -0500

    denoisnig version 3

--- data/watershed.py
+++ data/watershed.py
@@ -133,4 +133,4 @@ class WatershedFormatter(DataFormatter):
         Returns:
           Tuple of (training samples, validation samples)
         """
-        return 32000, 3840
+        return 3840, 3840
