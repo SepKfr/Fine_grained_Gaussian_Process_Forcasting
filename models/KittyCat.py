e07ead3303a5e89a6ddc7f44429a0e3f2c4cd628
sepidekoohfar <sepideh.koohfar@unh.edu>
2022-12-08 18:10:09 -0500

    simplify acat

--- models/KittyCat.py
+++ models/KittyCat.py
@@ -16,26 +16,20 @@ class KittyCatConv(nn.Module):
 
         self.device = device
         self.d_k = d_k
-        self.filter_length = [3, 7, 9]
-
-        self.proj_q = nn.Linear(d_k, 1, bias=False, device=device)
-        self.proj_k = nn.Linear(d_k, 1, bias=False, device=device)
+        self.filter_length = [1, 3, 7, 9]
 
         self.conv_list_k = nn.ModuleList([
-            nn.Sequential(nn.Conv1d(in_channels=h, out_channels=h, kernel_size=f, padding=int((f-1)/2)),
+            nn.Sequential(nn.Conv1d(in_channels=d_k*h, out_channels=h, kernel_size=f, padding=int((f-1)/2)),
                           nn.BatchNorm1d(h),
                           nn.Softmax(dim=-1))
             for f in self.filter_length
             ]).to(device)
 
         self.conv_list_q = nn.ModuleList([
-            nn.Sequential(nn.Conv1d(in_channels=h, out_channels=h, kernel_size=f, padding=int((f-1)/2)),
+            nn.Sequential(nn.Conv1d(in_channels=d_k*h, out_channels=h, kernel_size=f, padding=int((f-1)/2)),
                           nn.BatchNorm1d(h),
                           nn.Softmax(dim=-1))
             for f in self.filter_length]).to(device)
-
-        self.proj_back_q = nn.Linear(1, self.d_k, bias=False).to(device)
-        self.proj_back_k = nn.Linear(1, self.d_k, bias=False).to(device)
 
         self.factor = 1
 
@@ -46,23 +40,18 @@ class KittyCatConv(nn.Module):
         Q_l = []
         K_l = []
 
-        Q = self.proj_q(Q)
-        K = self.proj_k(K)
+        Q = Q.reshape(b, -1, l)
+        K = K.reshape(b, -1, l_k)
 
-        Q_l.append(self.conv_list_q[0](Q.reshape(b, -1, l)))
-        K_l.append(self.conv_list_k[0](K.reshape(b, -1, l_k)))
-
-        [Q_l.append(self.conv_list_q[i](Q_l[i-1])) for i in range(1, len(self.filter_length))]
-        [K_l.append(self.conv_list_k[i](K_l[i-1])) for i in range(1, len(self.filter_length))]
+        [Q_l.append(self.conv_list_q[i](Q)) for i in range(len(self.filter_length))]
+        [K_l.append(self.conv_list_k[i](K)) for i in range(len(self.filter_length))]
 
         Q_p = torch.cat(Q_l, dim=0).reshape(b, h, l * len(self.filter_length), -1)
         K_p = torch.cat(K_l, dim=0).reshape(b, h, l_k * len(self.filter_length), -1)
 
         Q = torch.topk(Q_p, l, dim=2)[0]
-        Q = self.proj_back_q(Q)
 
         K = torch.topk(K_p, l_k, dim=2)[0]
-        K = self.proj_back_k(K)
 
         scores = torch.einsum('bhqd,bhkd->bhqk', Q, K) / np.sqrt(self.d_k)
 
