1decc6989f9859d05e552e681179eaf4e4724a81
sepidekoohfar <sepideh.koohfar@unh.edu>
2022-12-12 18:47:44 -0500

    simplify acat

--- data/watershed.py
+++ data/watershed.py
@@ -133,4 +133,4 @@ class WatershedFormatter(DataFormatter):
         Returns:
           Tuple of (training samples, validation samples)
         """
-        return 3840, 3840
+        return 32000, 3840
