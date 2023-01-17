9e7f7594abbc28285112f3c0b3b6752a74361584
sepidekoohfar <sepideh.koohfar@unh.edu>
2023-01-17 11:53:54 -0500

    adding dae to rnn

--- train.py
+++ train.py
@@ -137,7 +137,7 @@ class Train:
         if not os.path.exists(self.model_path):
             os.makedirs(self.model_path)
 
-        d_model = trial.suggest_categorical("d_model", [16, 32])
+        d_model = trial.suggest_categorical("d_model", [128, 64])
         w_steps = trial.suggest_categorical("w_steps", [4000])
         stack_size = trial.suggest_categorical("stack_size", [1])
 
