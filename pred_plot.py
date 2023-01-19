99dc49cf07b9ed09a6ab1f6654e3968b64545998
sepidekoohfar <sepideh.koohfar@unh.edu>
2023-01-19 17:02:52 -0500

    get pred of autoformer

--- pred_plot.py
+++ pred_plot.py
@@ -165,7 +165,7 @@ for j in range(total_b*batch_size):
 
 inds.sort(reverse=True)
 
-for i in range(0, max(5, len(inds))):
+for i in range(0, min(5, len(inds))):
 
     plt.plot(np.arange(total_steps), tgt[inds[i]], color="gray")
     plt.plot(np.arange(total_steps-pred_len, total_steps), preds[inds[i]], color="lime")
