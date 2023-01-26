73bd7805837e29d5d991bc9b620a703b1d2c34b6
sepidekoohfar <sepideh.koohfar@unh.edu>
2023-01-25 19:32:48 -0500

    adding std dev

--- pred_plot.py
+++ pred_plot.py
@@ -17,7 +17,7 @@ from models.rnn import RNN
 
 plt.rc('font', size=8)
 plt.rc('axes', titlesize=8)
-plt.rcParams["figure.figsize"] = (12, 6)
+plt.rcParams["figure.figsize"] = (12,4)
 
 parser = argparse.ArgumentParser(description="preprocess argument parser")
 parser.add_argument("--attn_type", type=str, default='autoformer')
