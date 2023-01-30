1dfd2a84a59c2a8c84601cf34bb50f4a53f37e30
sepidekoohfar <sepideh.koohfar@unh.edu>
2023-01-30 17:18:20 -0500

    adding bash script to run

--- run.sh
+++ run.sh
@@ -1,4 +1,6 @@
 for seed in $[RANDOM%10000] $[RANDOM%10000] $[RANDOM%10000]
 do
-  python train.py --exp_name traffic --model_name ATA_gp --attn_type ATA --denoising True --gp True --seed "${seed}" --cuda cuda:0
+  python train.py --exp_name solar --model_name ATA_gp --attn_type ATA --denoising True --gp True --seed "${seed}" --cuda cuda:0
+  python train.py --exp_name solar --model_name ATA_iso --attn_type ATA --denoising True --gp False --seed "${seed}" --cuda cuda:0
+  python train.py --exp_name solar --model_name ATA_no --attn_type ATA --denoising False --gp False --seed "${seed}" --cuda cuda:0
 done
