6172251e14e80e61bee7d6ac2a4590ec9163bbe4
sepidekoohfar <sepideh.koohfar@unh.edu>
2023-02-01 08:29:16 -0500

    loading data from main dir

--- forecasting_models/ConvAttn.py
+++ forecasting_models/ConvAttn.py
@@ -20,22 +20,19 @@ class ConvAttn(nn.Module):
         self.device = device
         self.d_k = d_k
         self.conv_q = nn.Conv1d(in_channels=d_k*h, out_channels=d_k*h,
-                       kernel_size=kernel,
-                       padding=int(kernel/2), bias=False).to(device)
+                                kernel_size=kernel,
+                                padding=int(kernel/2), bias=False).to(device)
         self.conv_k = nn.Conv1d(in_channels=d_k * h, out_channels=d_k * h,
                                 kernel_size=kernel,
                                 padding=int(kernel / 2), bias=False).to(device)
-
-        self.norm = nn.BatchNorm1d(h * d_k).to(device)
-        self.activation = nn.ReLU().to(device)
 
     def forward(self, Q, K, V, attn_mask):
 
         b, h, l, d_k = Q.shape
         l_k = K.shape[2]
 
-        Q = self.activation(self.norm(self.conv_q(Q.reshape(b, h*d_k, l))))[:, :, :l].reshape(b, h, l, d_k)
-        K = self.activation(self.norm(self.conv_k(K.reshape(b, h*d_k, l_k))))[:, :, :l_k].reshape(b, h, l_k, d_k)
+        Q = self.conv_q(Q.reshape(b, h*d_k, l))[:, :, :l].reshape(b, h, l, d_k)
+        K = self.conv_k(K.reshape(b, h*d_k, l_k))[:, :, :l_k].reshape(b, h, l_k, d_k)
 
         scores = torch.einsum('bhqd,bhkd->bhqk', Q, K) / np.sqrt(self.d_k)
         attn = torch.softmax(scores, -1)
