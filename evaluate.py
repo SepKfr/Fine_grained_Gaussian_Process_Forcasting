be4c51dad32dcc0bc74c79d4549d98c0911c4299
sepidekoohfar <sepideh.koohfar@unh.edu>
2023-03-15 13:19:34 -0400

    multi-layer models

--- evaluate.py
+++ evaluate.py
@@ -44,7 +44,7 @@ data_csv_path = "{}.csv".format(args.exp_name)
 raw_data = pd.read_csv(data_csv_path)
 
 data = formatter.transform_data(raw_data)
-train_max, valid_max = formatter.get_num_samples_for_calibration()
+train_max, valid_max = formatter.get_num_samples_for_calibration(num_train=batch_size)
 max_samples = (train_max, valid_max)
 
 _, _, test = batch_sampled_data(data, 0.8, max_samples, params['total_time_steps'],
