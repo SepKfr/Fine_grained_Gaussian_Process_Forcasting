e7b02fc7bcc0678e0dd5a513ea5e4bb63bcecb4a
sepidekoohfar <sepideh.koohfar@unh.edu>
2023-03-15 13:10:44 -0400

    multi-layer models

--- evaluate.py
+++ evaluate.py
@@ -9,10 +9,9 @@ import torch
 import torch.nn as nn
 import os
 
-from Utils.base_train import batching, ModelData, batch_sampled_data
-from data.data_loader import ExperimentConfig
-from models.eff_acat import Transformer
-from models.rnn import RNN
+from Utils.base_train import batch_sampled_data
+from data_loader import ExperimentConfig
+from forecast_denoising import Forecast_denoising
 
 parser = argparse.ArgumentParser(description="preprocess argument parser")
 parser.add_argument("--attn_type", type=str, default='KittyCat')
@@ -22,6 +21,7 @@ parser.add_argument("--cuda", type=str, default="cuda:0")
 parser.add_argument("--pred_len", type=int, default=24)
 parser.add_argument("--dae", type=str, default="False")
 parser.add_argument("--gp", type=str, default="False")
+parser.add_argument("--no_noise", type=str, default="False")
 
 args = parser.parse_args()
 
@@ -70,6 +70,9 @@ n_batches_test = test_enc.shape[0]
 mse = nn.MSELoss()
 mae = nn.L1Loss()
 stack_size = 1
+denoising = True if args.dae == "True" else False
+gp = True if args.gp == "True" else False
+no_noise = True if args.no_noise == "True" else False
 
 
 for i, seed in enumerate([4293, 1692, 3029]):
@@ -78,56 +81,29 @@ for i, seed in enumerate([4293, 1692, 3029]):
             try:
                 d_k = int(d / n_heads)
 
-                if args.name == "lstm":
-                    p_model = False
-                    model = RNN(n_layers=stack_size,
-                                hidden_size=d,
-                                src_input_size=src_input_size,
-                                tgt_input_size=tgt_input_size,
-                                rnn_type="lstm",
-                                device=device,
-                                d_r=0,
-                                seed=seed,
-                                pred_len=pred_len)
-                else:
+                config = src_input_size, tgt_input_size, d_model, n_heads, d_k, stack_size
 
-                    p_model = True if args.dae == "True" else False
-                    gp = True if args.gp == "True" else False
-                    model = Transformer(src_input_size=src_input_size,
-                                        tgt_input_size=tgt_input_size,
-                                        pred_len=pred_len,
-                                        d_model=d,
-                                        d_ff=d * 4,
-                                        d_k=d_k, d_v=d_k, n_heads=n_heads,
-                                        n_layers=stack_size, src_pad_index=0,
-                                        tgt_pad_index=0, device=device,
-                                        attn_type=args.attn_type,
-                                        seed=seed, kernel=k,
-                                        p_model=p_model, gp=gp)
+                model = Forecast_denoising(model_name=args.name,
+                                           config=config,
+                                           gp=gp,
+                                           denoise=denoising,
+                                           device=device,
+                                           seed=seed,
+                                           pred_len=pred_len,
+                                           attn_type=args.attn_type,
+                                           no_noise=no_noise).to(device)
 
                 checkpoint = torch.load(os.path.join("models_{}_{}".format(args.exp_name, args.pred_len),
                                         "{}_{}".format(args.name, seed)))
                 state_dict = checkpoint['model_state_dict']
-                new_state_dict = OrderedDict()
-
-                if not p_model:
-                    for key, value in state_dict.items():
-                        if 'process' not in key:
-                            new_state_dict[key] = value
-
-                    model.load_state_dict(new_state_dict)
-                else:
-                    model.load_state_dict(checkpoint['model_state_dict'])
+                model.load_state_dict(state_dict)
 
                 model.eval()
                 model.to(device)
 
                 j = 0
                 for test_enc, test_dec, test_y in test:
-                    if p_model:
-                        output, _ = model(test_enc.to(device), test_dec.to(device))
-                    else:
-                        output = model(test_enc.to(device), test_dec.to(device))
+                    output, _ = model(test_enc.to(device), test_dec.to(device))
 
                     predictions[i, j] = output.squeeze(-1).cpu().detach().numpy()
                     if i == 0:
