a482fd75be7bd51c5cb859a86fda8e76ac0a51b6
sepidekoohfar <sepideh.koohfar@unh.edu>
2023-03-16 12:51:57 -0400

    multi-layer models

--- denoising_model/denoising_model.py
+++ denoising_model/denoising_model.py
@@ -40,9 +40,9 @@ class denoise_model(nn.Module):
         if self.gp:
             self.gp_proj = nn.Sequential(
                 nn.Conv1d(in_channels=1, out_channels=d, kernel_size=3, padding=int((3 - 1) / 2)),
-                nn.Conv1d(in_channels=d, out_channels=d, kernel_size=9, padding=int((9 - 1) / 2)),
+                nn.Conv1d(in_channels=d, out_channels=d, kernel_size=9, padding=int((3 - 1) / 2)),
                 nn.BatchNorm1d(d),
-                nn.ReLU()).to(device)
+                nn.Softmax(dim=-1)).to(device)
 
         self.d = d
         self.device = device
@@ -69,7 +69,7 @@ class denoise_model(nn.Module):
 
             dist = gpytorch.distributions.MultivariateNormal(mean, co_var)
             eps = self.gp_proj(dist.sample().unsqueeze(-1).permute(0, 2, 1)).permute(0, 2, 1)
-            x_noisy = self.norm(x + eps)
+            x_noisy = x + eps
 
         elif self.n_noise:
 
