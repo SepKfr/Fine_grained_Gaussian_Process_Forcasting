6c9c0b41bd86178f4b226cf712483d9516bb11dd
sepidekoohfar <sepideh.koohfar@unh.edu>
2023-03-16 20:28:06 -0400

    multi-layer models

--- denoising_model/denoising_model.py
+++ denoising_model/denoising_model.py
@@ -38,11 +38,17 @@ class denoise_model(nn.Module):
         self.residual = residual
 
         if self.gp:
-            self.gp_proj = nn.Sequential(
+            self.gp_proj_mean = nn.Sequential(
                 nn.Conv1d(in_channels=1, out_channels=d, kernel_size=3, padding=int((3 - 1) / 2)),
                 nn.Conv1d(in_channels=d, out_channels=d, kernel_size=3, padding=int((3 - 1) / 2)),
                 nn.BatchNorm1d(d),
                 nn.Softmax(dim=-1)).to(device)
+            self.gp_proj_var = nn.Sequential(
+                nn.Conv1d(in_channels=1, out_channels=d, kernel_size=3, padding=int((3 - 1) / 2)),
+                nn.Conv1d(in_channels=d, out_channels=d, kernel_size=3, padding=int((3 - 1) / 2)),
+                nn.BatchNorm1d(d),
+                nn.Softmax(dim=-1)
+            ).to(device)
 
         self.d = d
         self.device = device
@@ -68,7 +74,8 @@ class denoise_model(nn.Module):
             co_var = self.covar_module(x)
 
             dist = gpytorch.distributions.MultivariateNormal(mean, co_var)
-            eps = self.gp_proj(dist.sample().unsqueeze(-1).permute(0, 2, 1)).permute(0, 2, 1)
+            eps = self.gp_proj_mean(dist.mean.unsqueeze(-1).permute(0, 2, 1)).permute(0, 2, 1) + \
+                  self.gp_proj_var(dist.variance.unsqueeze(-1).permute(0, 2, 1)).permute(0, 2, 1) * eps
             x_noisy = self.norm(x + eps)
 
         elif self.n_noise:
