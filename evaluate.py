b5c62445f21c083c25f084aa45287880cedc449e
sepidekoohfar <sepideh.koohfar@unh.edu>
2023-03-24 13:30:19 -0500

    trying matren kernel

--- evaluate.py
+++ evaluate.py
@@ -120,7 +120,7 @@ predictions_mean = torch.from_numpy(np.mean(predictions, axis=0))
 predictions = torch.from_numpy(predictions)
 mse_std = torch.zeros(2, args.pred_len)
 
-for i in range(3):
+for i in range(2):
     for j in range(args.pred_len):
         mse_std[i, j] = mse(predictions[i, :, :, j], test_y_tot[:, :, j]).item()
 
