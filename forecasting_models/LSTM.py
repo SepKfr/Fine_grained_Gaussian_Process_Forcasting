ae2650d0fc68ca49a2f090c5fbe237f1b79adf9c
sepidekoohfar <sepideh.koohfar@unh.edu>
2023-04-16 13:18:50 -0500

    adding denoising as an option

--- forecasting_models/LSTM.py
+++ forecasting_models/LSTM.py
@@ -15,7 +15,8 @@ class RNN(nn.Module):
         random.seed(seed)
         torch.manual_seed(seed)
 
-        self.lstm = nn.LSTM(src_input_size, hidden_size, n_layers, dropout=d_r)
+        self.enc_lstm = nn.LSTM(src_input_size, hidden_size, n_layers, dropout=d_r)
+        self.dec_lstm = nn.LSTM(src_input_size, hidden_size, n_layers, dropout=d_r)
         self.n_layers = n_layers
         self.hidden_size = hidden_size
 
@@ -24,12 +25,12 @@ class RNN(nn.Module):
 
     def forward(self, x_en, x_de):
 
-        x = torch.cat((x_en, x_de), dim=1).permute(1, 0, 2)
+        enc_outputs, _ = self.lstm(x_en)
+        enc_outputs = enc_outputs.transpose(0, 1)
+        dec_outputs, _ = self.lstm(x_de)
+        dec_outputs = enc_outputs.transpose(0, 1)
 
-        outputs, _ = self.lstm(x)
-        outputs = outputs.transpose(0, 1)
-
-        return outputs
+        return enc_outputs, dec_outputs
 
 
 
