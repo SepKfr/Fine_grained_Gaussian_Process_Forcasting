90690cf9938d806e59111a8554cdccc9f2d946fa
sepidekoohfar <sepideh.koohfar@unh.edu>
2023-04-23 20:11:34 -0500

    adding dtw loss

--- train.py
+++ train.py
@@ -15,6 +15,7 @@ from optuna.trial import TrialState
 from data_loader import ExperimentConfig
 from Utils.base_train import batch_sampled_data
 from modules.opt_model import NoamOpt
+import pysdtw
 
 
 class Train:
@@ -47,6 +48,8 @@ class Train:
         self.param_history = []
         self.erros = dict()
         self.exp_name = args.exp_name
+        fun = pysdtw.distance.pairwise_l2_squared
+        self.sdtw = pysdtw.SoftDTW(gamma=0.5, dist_func=fun, use_cuda=False)
         self.best_model = nn.Module()
         self.train, self.valid, self.test = self.split_data()
         self.run_optuna(args)
@@ -145,7 +148,7 @@ class Train:
             for train_enc, train_dec, train_y in self.train:
 
                 output, kl_loss = model(train_enc.to(self.device), train_dec.to(self.device))
-                loss = nn.MSELoss()(output, train_y.to(self.device))
+                loss = self.sdtw(output, train_y.to(self.device)).sum()
 
                 total_loss += loss.item()
 
@@ -158,7 +161,7 @@ class Train:
             for valid_enc, valid_dec, valid_y in self.valid:
 
                 output, kl_loss = model(valid_enc.to(self.device), valid_dec.to(self.device))
-                loss = nn.MSELoss()(output, valid_y.to(self.device))
+                loss = self.sdtw(output, valid_y.to(self.device)).sum()
 
                 test_loss += loss.item()
 
