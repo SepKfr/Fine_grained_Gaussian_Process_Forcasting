c06bd7cc42cc9097ddeedcc4be6c7f7324f9a0f4
sepidekoohfar <sepideh.koohfar@unh.edu>
2023-04-26 11:40:51 -0500

    adding mll loss

--- data/watershed.py
+++ data/watershed.py
@@ -116,7 +116,7 @@ class WatershedFormatter(DataFormatter):
 
         model_params = {
             'hidden_layer_size': [8],
-            'minibatch_size': [512],
+            'minibatch_size': [256],
             'num_heads': 8,
             'stack_size': [1],
             'context_lengths': [1, 3, 6, 9]
