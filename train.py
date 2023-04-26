06832397b6c93a32962e69a80133a0b773b7f64b
sepidekoohfar <sepideh.koohfar@unh.edu>
2023-04-26 10:30:36 -0500

    adding mll loss

--- train.py
+++ train.py
@@ -167,7 +167,7 @@ class Train:
                     loss_gp = -mll(dist,
                                    torch.cat([torch.zeros(self.batch_size,
                                                           self.params['total_time_steps'] - 2*self.pred_len, 1),
-                                              train_y], dim=1)).mean()
+                                              train_y.to(self.device)], dim=1)).mean()
                 else:
                     loss_gp = 0
                 loss = nn.MSELoss()(output, train_y.to(self.device)) + 0.05 * loss_gp
