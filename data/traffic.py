282eddb472a57aa2a1ba3fad41fe5148f3c51d4f
sepidekoohfar <sepideh.koohfar@unh.edu>
2023-04-27 15:23:02 -0500

    adding deepGP

--- data/traffic.py
+++ data/traffic.py
@@ -169,7 +169,7 @@ class TrafficFormatter(GenericDataFormatter):
 
         model_params = {
             'hidden_layer_size': [8],
-            'minibatch_size': [512],
+            'minibatch_size': [256],
             'num_heads': 8,
             'stack_size': [1],
             'context_lengths': [1, 3, 6, 9]
