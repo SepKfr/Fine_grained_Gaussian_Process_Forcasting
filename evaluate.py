00df03a5fa1670741c29723624fde702a8936993
sepidekoohfar <sepideh.koohfar@unh.edu>
2023-04-30 15:07:58 -0500

    adding covid data

--- evaluate.py
+++ evaluate.py
@@ -123,7 +123,7 @@ for i, seed in enumerate([7631, 9873, 5249]):
                     j += 1
 
             except RuntimeError as e:
-                pass
+                print(e)
 
 predictions_mean = torch.from_numpy(np.mean(predictions, axis=0))
 predictions = torch.from_numpy(predictions)
