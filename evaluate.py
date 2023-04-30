885370b1cf39ed0ad57aa8ccb183fc7f22917c80
sepidekoohfar <sepideh.koohfar@unh.edu>
2023-04-30 16:16:21 -0500

    adding covid data

--- evaluate.py
+++ evaluate.py
@@ -114,7 +114,6 @@ for i, seed in enumerate([7631, 9873, 5249]):
                     for key, value in state_dict.items():
                         if "deep_gp" not in key:
                             if "mean_proj" not in key:
-                                print(key)
                                 new_state_dict[key] = value
 
                     state_dict = new_state_dict
