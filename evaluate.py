163e7b71245d0cc3c4cd80afbed3fce5eaeff12d
sepidekoohfar <sepideh.koohfar@unh.edu>
2023-04-30 14:12:29 -0500

    adding covid data

--- evaluate.py
+++ evaluate.py
@@ -105,8 +105,8 @@ for i, seed in enumerate([7631, 9873, 5249]):
                 checkpoint = torch.load(os.path.join("models_{}_{}".format(args.exp_name, args.pred_len),
                                         "{}_{}".format(args.name, seed)), map_location=device)
                 state_dict = checkpoint['model_state_dict']
-                print(state_dict)
                 model.load_state_dict(state_dict)
+                print(state_dict.keys())
 
                 model.eval()
 
