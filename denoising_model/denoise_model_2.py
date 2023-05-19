6607bf53c12c2d853df682bef72803d4d2ee49f2
sepidekoohfar <sepideh.koohfar@unh.edu>
2023-05-19 17:11:19 -0400

    decoder as many time steps as target

--- denoising_model/denoise_model_2.py
+++ denoising_model/denoise_model_2.py
@@ -49,8 +49,10 @@ class denoise_model_2(nn.Module):
         dist = None
 
         if self.gp:
-            enc_noisy, _ = self.add_gp_noise(enc_inputs)
-            dec_noisy, dist = self.add_gp_noise(dec_inputs)
+            inputs = torch.cat([enc_inputs, dec_inputs], dim=1)
+            input_noisy, dist = self.add_gp_noise(inputs)
+            enc_noisy = input_noisy[:, :enc_inputs.shape[1], :]
+            dec_noisy = input_noisy[:, -enc_inputs.shape[1]:, :]
 
         elif self.n_noise:
 
