a7ebb4672194c9ff0722138fd088a05e92bae6b0
sepidekoohfar <sepideh.koohfar@unh.edu>
2023-05-24 11:32:13 -0400

    set model.eval in evaluation

--- forecast_denoising.py
+++ forecast_denoising.py
@@ -65,7 +65,7 @@ class Forecast_denoising(nn.Module):
         enc_inputs = self.enc_embedding(enc_inputs)
         dec_inputs = self.dec_embedding(dec_inputs)
 
-        if self.input_corrupt:
+        if self.input_corrupt and not self.training:
             enc_outputs, dec_outputs = self.forecasting_model(enc_inputs, dec_inputs)
 
         else:
