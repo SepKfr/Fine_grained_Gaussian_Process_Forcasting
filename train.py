213f090ddbb392868a5adbba8fd9a819bbac7193
sepidekoohfar <sepideh.koohfar@unh.edu>
2023-07-08 12:02:43 -0500

    updating ATA

--- train.py
+++ train.py
@@ -279,7 +279,7 @@ def main():
     data_csv_path = "{}.csv".format(args.exp_name)
     raw_data = pd.read_csv(data_csv_path, dtype={'date': str})
 
-    for pred_len in [24]:
+    for pred_len in [48, 72, 96]:
         Train(raw_data, args, pred_len)
 
 
