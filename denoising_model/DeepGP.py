5ee889f9737ff1355ac51c42cf5768b6ba607859
sepidekoohfar <sepideh.koohfar@unh.edu>
2023-09-18 19:57:41 -0400

    adding NHits

--- denoising_model/DeepGP.py
+++ denoising_model/DeepGP.py
@@ -78,7 +78,7 @@ class DeepGPp(DeepGP):
         hidden_layer = ToyDeepGPHiddenLayer(
             input_dims=num_hidden_dims,
             output_dims=None,
-            mean_type='constant',
+            mean_type='linear',
             seed=seed,
         )
 
