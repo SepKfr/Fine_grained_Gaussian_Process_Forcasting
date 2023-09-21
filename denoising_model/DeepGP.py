51c067fc7762885d976347acc792fe188aaa9c54
sepidekoohfar <sepideh.koohfar@unh.edu>
2023-09-21 10:38:38 -0400

    add MSE and MAE for DeepAR without producing predictions

--- denoising_model/DeepGP.py
+++ denoising_model/DeepGP.py
@@ -12,7 +12,7 @@ from gpytorch.variational import VariationalStrategy, MeanFieldVariationalDistri
 
 
 class ToyDeepGPHiddenLayer(DeepGPLayer):
-    def __init__(self, nu, input_dims, output_dims, seed, num_inducing=32, mean_type='constant'):
+    def __init__(self, nu, input_dims, output_dims, seed, num_inducing=128, mean_type='constant'):
 
         np.random.seed(seed)
         random.seed(seed)
