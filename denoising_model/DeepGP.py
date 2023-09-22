be4814a7319bb6a6b97f4302b3f7961a9c3bda20
sepidekoohfar <sepideh.koohfar@unh.edu>
2023-09-22 17:56:45 -0500

    back with 1 tasks

--- denoising_model/DeepGP.py
+++ denoising_model/DeepGP.py
@@ -44,11 +44,7 @@ class ToyDeepGPHiddenLayer(DeepGPLayer):
             self.mean_module = ConstantMean(batch_shape=batch_shape)
         else:
             self.mean_module = LinearMean(input_dims)
-        try:
-            kernel = RBFKernel(batch_shape=batch_shape, ard_num_dims=input_dims)\
-                     * MaternKernel(batch_shape=batch_shape, ard_num_dims=input_dims, nu=0.5)
-        except RuntimeError:
-            kernel = RBFKernel(batch_shape=batch_shape, ard_num_dims=input_dims)
+        kernel = RBFKernel(batch_shape=batch_shape, ard_num_dims=input_dims)
 
         self.covar_module = ScaleKernel(
             kernel,
--- denoising_model/denoise_model_2.py
+++ denoising_model/denoise_model_2.py
@@ -44,7 +44,7 @@ class denoise_model_2(nn.Module):
         eps_gp = torch.cat([dist.sample() for _ in range(self.d)])
         eps_gp = eps_gp.reshape(b, s, -1)
 
-        x_noisy = x + eps_gp
+        x_noisy = self.norm1(x + self.ffn(eps_gp))
 
         return x_noisy, dist
 
