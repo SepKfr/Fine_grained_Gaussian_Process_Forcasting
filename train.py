c0668562438e4f82b25e519d6778a35802f2b363
sepidekoohfar <sepideh.koohfar@unh.edu>
2023-09-29 09:44:19 -0500

    moving to previous set up

--- train.py
+++ train.py
@@ -114,9 +114,9 @@ with gpytorch.settings.num_likelihood_samples(1):
 
             # hyperparameters
 
-            d_model = trial.suggest_categorical("d_model", [32])
+            d_model = trial.suggest_categorical("d_model", [16, 32])
 
-            w_steps = trial.suggest_categorical("w_steps", [1000])
+            w_steps = trial.suggest_categorical("w_steps", [1000, 8000])
             stack_size = trial.suggest_categorical("stack_size", [1, 2])
 
             n_heads = self.model_params['num_heads']
