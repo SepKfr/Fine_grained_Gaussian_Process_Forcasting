36e0c0a08bf846e41846e0c005403f7bc10c967d
sepidekoohfar <sepideh.koohfar@unh.edu>
2023-10-05 12:12:47 -0400

    sample for Gaussian instead of projecting up

--- denoising_model/DeepGP.py
+++ denoising_model/DeepGP.py
@@ -8,7 +8,7 @@ from gpytorch.kernels import ScaleKernel, RBFKernel, MaternKernel
 from gpytorch.likelihoods import GaussianLikelihood
 from gpytorch.means import ConstantMean, LinearMean
 from gpytorch.models.deep_gps import DeepGPLayer, DeepGP
-from gpytorch.variational import VariationalStrategy, MeanFieldVariationalDistribution
+from gpytorch.variational import VariationalStrategy, CholeskyVariationalDistribution
 
 
 class ToyDeepGPHiddenLayer(DeepGPLayer):
@@ -25,7 +25,7 @@ class ToyDeepGPHiddenLayer(DeepGPLayer):
             inducing_points = torch.randn(output_dims, num_inducing, input_dims)
             batch_shape = torch.Size([output_dims])
 
-        variational_distribution = MeanFieldVariationalDistribution(
+        variational_distribution = CholeskyVariationalDistribution(
             num_inducing_points=num_inducing,
             batch_shape=batch_shape
         )
@@ -47,7 +47,6 @@ class ToyDeepGPHiddenLayer(DeepGPLayer):
             RBFKernel(batch_shape=batch_shape, ard_num_dims=input_dims),
             batch_shape=batch_shape, ard_num_dims=None
         )
-
 
     def forward(self, x):
         mean_x = self.mean_module(x)
