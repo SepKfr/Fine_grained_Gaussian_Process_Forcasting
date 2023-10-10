56065d29f49e9f0d86dcce6397146d11f6c9b274
sepidekoohfar <sepideh.koohfar@unh.edu>
2023-10-10 19:48:16 -0400

    new setup

--- denoising_model/denoise_model_2.py
+++ denoising_model/denoise_model_2.py
@@ -34,7 +34,7 @@ class denoise_model_2(nn.Module):
 
         dist = self.deep_gp(x)
         eps_gp = torch.cat([dist.sample().permute(1, 2, 0) for _ in range(self.d)], dim=-1)
-        x_noisy = x + eps_gp
+        x_noisy = x + eps_gp * 0.05
 
         return x_noisy, dist
 
