5a991bbb310db8f95506afe3c980845ee671189c
sepidekoohfar <sepideh.koohfar@unh.edu>
2023-11-20 15:26:40 -0500

    running AutoDWC

--- forecasting-figs.py
+++ forecasting-figs.py
@@ -50,7 +50,7 @@ data = formatter.transform_data(raw_data)
 train_max, valid_max = formatter.get_num_samples_for_calibration(num_train=batch_size)
 max_samples = (train_max, valid_max)
 
-_, _, test = batch_sampled_data(data, 0.1, max_samples, params['total_time_steps'],
+_, _, test = batch_sampled_data(data, 0.8, max_samples, params['total_time_steps'],
                                         params['num_encoder_steps'], pred_len,
                                         params["column_definition"],
                                         batch_size)
