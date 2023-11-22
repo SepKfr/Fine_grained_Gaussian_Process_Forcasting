633150229d58ee79bd59a93e815e67c9818459ad
sepidekoohfar <sepideh.koohfar@unh.edu>
2023-11-22 11:09:04 -0500

    Including AutoDWC

--- evaluate.py
+++ evaluate.py
@@ -158,7 +158,7 @@ for i in range(1):
 mse_loss = mse_std_mean.mean(dim=0)
 mae_loss = mae_std_mean.mean(dim=0)
 print(mse_loss)
-print(mse_loss)
+print(mae_loss)
 mse_std = torch.std(mse_std_mean.mean(dim=-1)) / np.sqrt(pred_len)
 mae_std = torch.std(mae_loss.mean(dim=-1)) / np.sqrt(pred_len)
 
