a00943b0f54c4d435ed9210b7d09ba835a24a476
sepidekoohfar <sepideh.koohfar@unh.edu>
2024-05-31 20:18:38 -0500

    previous setup for ATA

--- train.py
+++ train.py
@@ -248,6 +248,7 @@ with gpytorch.settings.num_likelihood_samples(1):
 
         parser = argparse.ArgumentParser(description="preprocess argument parser")
         parser.add_argument("--attn_type", type=str, default='ATA')
+        parser.add_argument("--model_name", type=str, default='ATA')
         parser.add_argument("--exp_name", type=str, default='exchange')
         parser.add_argument("--cuda", type=str, default="cuda:0")
         parser.add_argument("--seed", type=int, default=1234)
