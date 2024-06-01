ea91ebfa8d4bc2d4b21446fab392e0f1fee5f9e2
sepidekoohfar <sepideh.koohfar@unh.edu>
2024-06-01 14:28:28 -0500

    previous setup for ATA

--- data/solar.py
+++ data/solar.py
@@ -51,7 +51,7 @@ class SolarFormatter(ElectricityFormatter):
 
         model_params = {
             'hidden_layer_size': [8],
-            'minibatch_size': [512],
+            'minibatch_size': [256],
             'num_heads': 8,
             'stack_size': [1],
             'context_lengths': [1, 3, 6, 9]
